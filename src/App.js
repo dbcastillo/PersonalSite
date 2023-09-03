c33fc12f10fcfb4eb4cd32f9c1249dc82720be13
Derick Castillo <9derick@gmail.com>
2023-09-02 20:27:59 -0500

    Add responsive nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import styled from 'styled-components'
+import Header from './components/Banner/Header'
+
 function App() {
   return (
-    <div>Hello world</div>
+    <Container>
+      <Banner>
+        <Header/>
+      </Banner>
+    </Container>
   )
 }
 
 export default App
+
+const Container = styled.div``
+const Banner = styled.div`
+background: linear-gradient(159deg, rgb(45, 45, 58) 0%, rgb(43, 43, 53) 100%);
+`
